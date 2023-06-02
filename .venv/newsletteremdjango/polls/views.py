b6cb7560cae51d1c14984760fd15f99c9fb59c51
Matheus Henrique <134849202+MatheusHFerreiraLima@users.noreply.github.com>
2023-06-02 17:06:31 -0300

    Consegui fazer o For no HTML
    Fiz isso por meio de um for no HTML que itera sobre uma lista, e cada iteração tem um dicionário com valores. Agora falta ler esses dados automaticamente.

--- .venv/newsletteremdjango/polls/views.py
+++ .venv/newsletteremdjango/polls/views.py
@@ -4,6 +4,15 @@ from django.shortcuts import render
 
 from django.http import  HttpResponse
 
-def index(request):
-    return HttpResponse("Olá, mundo! Eu estou lascado no meu TCC e o tempo está se encurtando.")
-    #"Esta é a view mais simples possível no Django". Se eu não estou entendedo isso e nem python básico, imagina as mais complexas.
+def dadosemailview(request):
+    dados_list = [
+        {'urlnoticia': 'https://facebook.com', 'noticiatitulo': 'olha a pedra', 'urlimg': 'https://raw.githubusercontent.com/MatheusHFerreiraLima/HTML-CSS/main/imagens/AUTO-F1-ITA-FERRARI-SF-23.webp' , 'imgdescricao': 'descriçao imagem teste', 'noticiadescricao': 'AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!'}, 
+
+        {'urlnoticia': 'https://facebook.com', 'noticiatitulo': 'olha a pedra', 'urlimg': 'https://raw.githubusercontent.com/MatheusHFerreiraLima/HTML-CSS/main/imagens/AUTO-F1-ITA-FERRARI-SF-23.webp' , 'imgdescricao': 'descriçao imagem teste', 'noticiadescricao': 'HAHHAHAHHHHHHHHHHHHHHHHHH'},
+
+        {'urlnoticia': None, 'noticiatitulo': None, 'urlimg': None , 'imgdescricao': None, 'noticiadescricao': None},
+        ]
+    return render(request, 'polls/email.html', {'dados': dados_list})
+
+
+    
--- arquivo1.py
+++ arquivo1.py
@@ -1,1 +1,9 @@
 print('Esse é só um teste inicial de um arquivo simples em !Python.')
+
+dadoss_list = [ 
+    {'urlnoticia': 'https://facebook.com', 'noticiatitulo': 'olha a pedra', 'urlimg': 'https://raw.githubusercontent.com/MatheusHFerreiraLima/HTML-CSS/main/imagens/AUTO-F1-ITA-FERRARI-SF-23.webp' , 'imgdescricao': 'descriçao imagem teste', 'noticiadescricao': 'Deus é fiel!'}, 
+
+    {'urlnoticia': 'https://facebook.com', 'noticiatitulo': 'olha a pedra', 'urlimg': 'https://raw.githubusercontent.com/MatheusHFerreiraLima/HTML-CSS/main/imagens/AUTO-F1-ITA-FERRARI-SF-23.webp' , 'imgdescricao': 'descriçao imagem teste', 'noticiadescricao': 'Deus é fiel!'}
+]
+
+print(dadoss_list)
